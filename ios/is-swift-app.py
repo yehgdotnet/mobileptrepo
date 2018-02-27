d38e4d53da408f8741847687ac8124bba36a0790
Myo Soe <dan.myo@cybersecurityassessor.ninja>
2018-02-28 02:51:03 +0800

    Added is-swift-app of Myo Soe

---
+++ ios/is-swift-app.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+
+# Author: Myo Soe, https://yehg.net 
+
+# requires python 3, nm, otool
+# decryped ipa file as 1st argument 
+
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("file", type=str, help="Specify path to decrypted iOS app file")
+args = parser.parse_args()
+
+path = args.file
+path = path.strip()
+
+nm_cmd = ['nm', path]
+
+r_nm = subprocess.run(nm_cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+if r_nm.find ("_OBJC_CLASS_$__") > -1:
+    otool_cmd = ['otool', "-L" , path]
+    r_otool = subprocess.run(otool_cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    if r_otool.find("libswift")>-1:
+        print("The app was written in Swift.")
+        
